7e530350b95adc93889788dcd350f8cf79bb34e9
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-05-26 19:13:41 -0300

    add images continents

--- src/components/ContinenSlide/index.tsx
+++ src/components/ContinenSlide/index.tsx
@@ -2,19 +2,22 @@ import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
-// import "swiper/css";
-// import "swiper/css/pagination";
-// import "swiper/css/navigation";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 import styles from "./styles.module.css";
 
 // import required modules
 import { Keyboard, Pagination, Navigation } from "swiper";
 
+
 export const ContinentSlide = () => {
+
+
   return (
     <>
+
       <Swiper
         slidesPerView={1}
         spaceBetween={30}
@@ -28,15 +31,12 @@ export const ContinentSlide = () => {
         modules={[Keyboard, Pagination, Navigation]}
         className={styles.swiper}
       >
-        <SwiperSlide className={styles.swiperSlide}>Slide 1</SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}>Slide 2</SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}>Slide 3</SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}>Slide 4</SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}>Slide 5</SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}>Slide 6</SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}>Slide 7</SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}>Slide 8</SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}>Slide 9</SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide} ><img src='./images/northAmerica.jpg'></img></SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide}><img src='./images/southAmerica.jpg'></img></SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide}><img src='./images/asia.jpg'></img></SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide}><img src='./images/africa.jpg'></img></SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide}><img src='./images/europe.jpg'></img></SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide}><img src='./images/oceania.jpg'></img></SwiperSlide>
       </Swiper>
     </>
   );
