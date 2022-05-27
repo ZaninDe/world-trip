04eab7f7b701ae9a5c567d94f6d0c9d81f739c70
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-05-27 18:37:36 -0300

    pointer cursor on the header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Image } from "@chakra-ui/react"
 import Link from "next/link"
 
 export const Header = () => {
   return (
     <Box display="flex" alignItems="center" justifyContent="center" w="100%" h="100px">
-      <Link href='/'><img src="./images/Logo.svg" alt="worldtrip logo" /></Link>
+      <Link href='/'><Image src="./images/Logo.svg" alt="worldtrip logo" cursor='pointer'/></Link>
     </Box>
   )
 }
