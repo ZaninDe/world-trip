29cf7525c30b5fdcfe3be6a50c6c95fc8df66ca5
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-05-31 17:35:35 -0300

    axios fetching data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,29 @@ import { Header } from '../components/Header'
 import { Banner } from '../components/Banner'
 import { ContinentSlide } from '../components/ContinenSlide'
 
+import axios, { AxiosResponse } from 'axios'
+
+
 const Home: NextPage = () => {
+
+  async function getUser<PromiseResult>() {
+    try {
+     const user = await axios.get('https://api.github.com/users/ZaninDe').then(
+      res => res.data
+     );
+      console.log(user.login);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <>
+      {/* <h1>{user.data.login}</h1> */}
       <Banner />
       <NavTravelTypes />
       <ContinentSlide />
+      <button onClick={getUser}>click me</button>
     </>
   )
 }
